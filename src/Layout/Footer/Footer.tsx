d9333070e008cb87f40db057e2dd55c2e4d51605
edytaskibinska <edyta.skibinska@gmail.com>
2023-07-19 12:38:23 +0200

    Modal implementations from @e-skibinska/proton-design-system

--- src/Layout/Footer/Footer.tsx
+++ src/Layout/Footer/Footer.tsx
@@ -1,10 +1,13 @@
-import { FC, ReactNode } from "react";
+import { FC, ReactNode, useState } from "react";
 import "./Footer.scss";
 
 import { useSelector, useDispatch } from "react-redux";
 import { RootState } from "../../Store/configureStore";
 import {} from "react-redux";
 import { emptyCart } from "../../Store/reducers/cartSlice";
+
+//Modal from npm design system
+import { Modal } from "@e-skibinska/proton-design-system";
 
 interface IFooter {
   children?: ReactNode;
@@ -13,17 +16,32 @@ interface IFooter {
 const Footer: FC<IFooter> = ({ children }) => {
   const cartItems = useSelector((state: RootState) => state.cart.items);
   const dispatch = useDispatch();
+  const [confirmation, setConfirmation] = useState(false);
 
   const handleEmptyCart = (stateArray: any) => {
     dispatch(emptyCart(stateArray));
+    setConfirmation(true);
   };
   return (
-    cartItems.length && (
-      <footer onClick={() => handleEmptyCart(cartItems)} className="footer">
-     
+    <>
+      {cartItems.length && (
+        <footer onClick={() => handleEmptyCart(cartItems)} className="footer">
           <p className="footer__text">{children}</p>
-      </footer>
-    )
+        </footer>
+      )}
+      <Modal
+        onClick={() => setConfirmation(false)}
+        isOpen={confirmation}
+        className="modal"
+        modalTitle="Le panier est vide"
+        buttonText="OK"
+        buttoncolor="#7861c1"
+        modaltextcolor="#dd3d62"
+        withButtonClose
+      >
+        Votre panier a été vidé, nous vous invitons d'ajouter des produits :)
+      </Modal>
+    </>
   );
 };
 
