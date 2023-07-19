05466c7623e14c03acf465d5e9849c929cc4c8fb
edytaskibinska <edyta.skibinska@gmail.com>
2023-07-19 12:02:53 +0200

    WIP : Header / Footer features

--- src/Layout/Footer/Footer.tsx
+++ src/Layout/Footer/Footer.tsx
@@ -1,15 +1,22 @@
 import { FC, ReactNode } from "react";
-import "./Footer.scss"
+import "./Footer.scss";
+
+import { useSelector } from "react-redux";
+import { RootState } from "../../Store/configureStore";
 
 interface IFooter {
   children?: ReactNode;
 }
 
 const Footer: FC<IFooter> = ({ children }) => {
+  const cartItems = useSelector((state: RootState) => state.cart.items);
+
   return (
-    <footer className="footer">
-      <p className="footer__text">{children}</p>
-    </footer>
+    cartItems.length && (
+      <footer className="footer">
+        <p className="footer__text">{children}</p>
+      </footer>
+    )
   );
 };
 
